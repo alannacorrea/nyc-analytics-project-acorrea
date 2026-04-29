ce7d49ebe280d017914cf87f126a8ceea7917867
Alanna Correa <alanna.correa@baruchmail.cuny.edu>
2026-04-29 21:08:36 +0000

    created dim street segment table

---
+++ models/project_work/marts/street_pavement_rating/dim_street_segment.sql
@@ -0,0 +1,24 @@
+--  dimension for street location
+WITH street_segement AS (
+   SELECT DISTINCT
+      oftcode, 
+      onstreetna, 
+      fromstreet, 
+      tostreetna, 
+      direction, 
+      road_type, 
+      ismultipass,
+   FROM {{ ref('stg_street_pavement_rating') }}
+   WHERE oftcode IS NOT NULL
+),
+
+street_dimension AS (
+   SELECT
+       {{ dbt_utils.generate_surrogate_key([
+           'oftcode'
+       ]) }} AS street_dimension_key,
+      *
+     FROM street_segement
+)
+
+SELECT * FROM street_dimension
